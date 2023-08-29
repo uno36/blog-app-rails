89230098a96d96acceb729c81e70866331105ba0
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-08-29 15:48:16 +0200

    Correct the requirements

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,9 @@
-class PostsController < ActionController::Base
-  def index; end
-
-  def show; end
-
-  def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to posts_path
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
   end
 
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to posts_path
-  end
-
-  private
-
-  def post_params
-    paramst.require(:post).permit(:author_id)
+  def show
+    @post = 'post'
   end
 end
