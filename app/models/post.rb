992977df608dd998e3cd313e60757b795e69cfc8
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-01 09:25:30 +0200

    Create tasks

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,13 +9,13 @@ class Post < ApplicationRecord
 
   after_save :update_user_posts_counter
 
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
+
   private
 
   def update_user_posts_counter
     author.increment!(:posts_counter)
-  end
-
-  def recent_comments(limit = 5)
-    comments.order(created_at: :desc).limit(limit)
-  end
+  end  
 end
