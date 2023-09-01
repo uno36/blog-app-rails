124a8b36cccf7d5ce7cb3280d15f47ed57afd93e
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-01 19:19:06 +0200

    Add methods to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,27 @@
-class UsersController < ApplicationController
-  def show_comments
-    @user = User.find(params[:id])
-    @comments = @user.comments
+class CommentsController < ApplicationController
+  def new
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.author = @user
+    if @comment.save
+      flash[:success] = 'Comment created successfully'
+      redirect_to user_post_path(@user, @post)
+    else
+      flash.now[:errors] = 'Error: Comment could not be saved'
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
